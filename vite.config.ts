228d0ae6540f24fa3429b8b358d9fabc2df4b500
ysyyds1688-maker <ysyyds1688@gmail.com>
2025-11-28 15:31:14 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -17,10 +17,10 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       }
+    }
     };
 });
